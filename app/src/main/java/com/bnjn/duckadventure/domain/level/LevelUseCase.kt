0b9c5dc742b4700e08e1b2649b0c26b224a54206
bnjn-mt <benjamin.johnson@madetech.com>
2025-12-07 16:51:01 +0000

    Added perimeter walls and changed player start to always be in bounds

--- app/src/main/java/com/bnjn/duckadventure/domain/level/LevelUseCase.kt
+++ app/src/main/java/com/bnjn/duckadventure/domain/level/LevelUseCase.kt
@@ -2,15 +2,26 @@ package com.bnjn.duckadventure.domain.level
 
 import com.bnjn.duckadventure.domain.level.models.LevelGrid
 import com.bnjn.duckadventure.domain.level.models.Player
+import com.bnjn.duckadventure.domain.level.models.Wall
 import kotlin.random.Random
 
 class LevelUseCase(
     levelGrid: LevelGrid,
 ) {
     private var level: Array<Array<Any?>> = levelGrid.grid
-    private var playerPosition: Pair<Int, Int> = Pair(Random.nextInt(0, level.size), Random.nextInt(0, level[0].size))
+    private var playerPosition: Pair<Int, Int> = Pair(Random.nextInt(1, level.size - 1), Random.nextInt(1, level[0].size - 1))
 
-    fun execute() {
+    init {
+        // Set up perimeter Walls
+        level.first().forEachIndexed { index, row -> level.first()[index] = Wall() }
+        level.last().forEachIndexed { index, row -> level.last()[index] = Wall() }
+        level.forEachIndexed { index, row ->
+            if (index != 0 && index != level.lastIndex) {
+                row[0] = Wall()
+                row[row.lastIndex] = Wall()
+            }
+        }
+
         // Set up level with a Player
         level[playerPosition.first][playerPosition.second] = Player()
     }
--- app/src/test/java/com/bnjn/duckadventure/domain/level/LevelUseCaseTest.kt
+++ app/src/test/java/com/bnjn/duckadventure/domain/level/LevelUseCaseTest.kt
@@ -2,22 +2,20 @@ package com.bnjn.duckadventure.domain.level
 
 import com.bnjn.duckadventure.domain.level.models.LevelGrid
 import com.bnjn.duckadventure.domain.level.models.Player
+import com.bnjn.duckadventure.domain.level.models.Wall
 import org.junit.Assert.assertTrue
 import org.junit.Test
-import java.lang.Thread.sleep
 
 class LevelUseCaseTest {
     @Test
     fun `getLevel should return a level containing a single Player object when LevelUseCase is init with a LevelGrid`() {
         val levelUseCase = LevelUseCase(LevelGrid(10, 10))
-        levelUseCase.execute()
         assertTrue(levelUseCase.getLevel().flatten().count { it is Player } == 1)
     }
 
     @Test
     fun `getPlayerPosition should return a valid player position as a pair`() {
         val levelUseCase = LevelUseCase(LevelGrid(10, 10))
-        levelUseCase.execute()
         val playerPosition: Pair<Int, Int> = levelUseCase.getPlayerPosition()
 
         assertTrue(playerPosition.first <= 10)
@@ -28,11 +26,34 @@ class LevelUseCaseTest {
 
     @Test
     fun `the Player should start at a random position`() {
-        val levelUseCase = LevelUseCase(LevelGrid(100, 100))
-        levelUseCase.execute()
-        val anotherLevelUseCase = LevelUseCase(LevelGrid(100, 100))
-        anotherLevelUseCase.execute()
+        val levelUseCase = LevelUseCase(LevelGrid(1000, 1000))
+        val anotherLevelUseCase = LevelUseCase(LevelGrid(1000, 1000))
+
         assertTrue(levelUseCase.getPlayerPosition().first != anotherLevelUseCase.getPlayerPosition().first)
         assertTrue(levelUseCase.getPlayerPosition().second != anotherLevelUseCase.getPlayerPosition().second)
+    }
+
+    @Test
+    fun `the LevelGrid should have a Wall at each perimeter position`() {
+        val levelUseCase = LevelUseCase(LevelGrid(20, 20))
+        val level = levelUseCase.getLevel()
+
+        assertTrue(level.first().all { it is Wall })
+        assertTrue(level.last().all { it is Wall })
+        assertTrue(level.map { it.first() }.all { it is Wall })
+        assertTrue(level.map { it.last() }.all { it is Wall })
+    }
+
+    @Test
+    fun `a Player can't start where a Wall should be`() {
+        val levelUseCase = LevelUseCase(LevelGrid(3, 3))
+        val level = levelUseCase.getLevel()
+
+        assertTrue(level.first().all { it is Wall })
+        assertTrue(level.last().all { it is Wall })
+        assertTrue(level.map { it.first() }.all { it is Wall })
+        assertTrue(level.map { it.last() }.all { it is Wall })
+        assertTrue(levelUseCase.getPlayerPosition().first == 1)
+        assertTrue(levelUseCase.getPlayerPosition().second == 1)
     }
 }
