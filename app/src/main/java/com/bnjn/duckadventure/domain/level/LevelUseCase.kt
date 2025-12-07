da30d954fac1fbb01b6bfa8b91344e4bf8d0024c
bnjn-mt <benjamin.johnson@madetech.com>
2025-12-07 15:49:51 +0000

    Random player starting position

--- app/src/main/java/com/bnjn/duckadventure/domain/level/LevelUseCase.kt
+++ app/src/main/java/com/bnjn/duckadventure/domain/level/LevelUseCase.kt
@@ -2,16 +2,20 @@ package com.bnjn.duckadventure.domain.level
 
 import com.bnjn.duckadventure.domain.level.models.LevelGrid
 import com.bnjn.duckadventure.domain.level.models.Player
+import kotlin.random.Random
 
 class LevelUseCase(
     levelGrid: LevelGrid,
 ) {
     private var level: Array<Array<Any?>> = levelGrid.grid
+    private var playerPosition: Pair<Int, Int> = Pair(Random.nextInt(0, level.size), Random.nextInt(0, level[0].size))
 
     fun execute() {
         // Set up level with a Player
-        level[0][0] = Player()
+        level[playerPosition.first][playerPosition.second] = Player()
     }
 
     fun getLevel(): Array<Array<Any?>> = level
+
+    fun getPlayerPosition(): Pair<Int, Int> = playerPosition
 }
--- app/src/test/java/com/bnjn/duckadventure/domain/level/LevelUseCaseTest.kt
+++ app/src/test/java/com/bnjn/duckadventure/domain/level/LevelUseCaseTest.kt
@@ -4,12 +4,35 @@ import com.bnjn.duckadventure.domain.level.models.LevelGrid
 import com.bnjn.duckadventure.domain.level.models.Player
 import org.junit.Assert.assertTrue
 import org.junit.Test
+import java.lang.Thread.sleep
 
 class LevelUseCaseTest {
     @Test
-    fun `getLevel should return a level containing a single Player object when LevelUseCase is created with a LevelGrid`() {
+    fun `getLevel should return a level containing a single Player object when LevelUseCase is init with a LevelGrid`() {
         val levelUseCase = LevelUseCase(LevelGrid(10, 10))
         levelUseCase.execute()
         assertTrue(levelUseCase.getLevel().flatten().count { it is Player } == 1)
+    }
+
+    @Test
+    fun `getPlayerPosition should return a valid player position as a pair`() {
+        val levelUseCase = LevelUseCase(LevelGrid(10, 10))
+        levelUseCase.execute()
+        val playerPosition: Pair<Int, Int> = levelUseCase.getPlayerPosition()
+
+        assertTrue(playerPosition.first <= 10)
+        assertTrue(playerPosition.second <= 10)
+        assertTrue(playerPosition.first >= 0)
+        assertTrue(playerPosition.second >= 0)
+    }
+
+    @Test
+    fun `the Player should start at a random position`() {
+        val levelUseCase = LevelUseCase(LevelGrid(100, 100))
+        levelUseCase.execute()
+        val anotherLevelUseCase = LevelUseCase(LevelGrid(100, 100))
+        anotherLevelUseCase.execute()
+        assertTrue(levelUseCase.getPlayerPosition().first != anotherLevelUseCase.getPlayerPosition().first)
+        assertTrue(levelUseCase.getPlayerPosition().second != anotherLevelUseCase.getPlayerPosition().second)
     }
 }
